30584f12406bef84523a9804630732bcfd545655
Kushagra Kumar <167355875+KushagraKumar04@users.noreply.github.com>
2024-08-16 21:07:32 +0530

    n

---
+++ bot.py
@@ -0,0 +1,13 @@
+import os
+
+import telebot
+
+BOT_TOKEN = os.environ.get('7403031725:AAG-PjaJYDGVxpTCoJNJHSsfNZmis4LJbQM')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+@ bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
