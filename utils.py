b650f041482204d65e97e9a7cf077da0fa0e41bb
Kushagra Kumar <167355875+KushagraKumar04@users.noreply.github.com>
2024-08-19 21:18:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ def get_daily_horoscope(sign, day):
     # For simplicity, this example uses a static response.
     # You should replace this with an actual API call or database query.
     horoscope_data = {
-        "Aries": "Today is a great day to on personal growth.",
-        "Taurus": "You might find unexpected financial gains today.",
+        "Aries": "Today is a great day to on personal growthh.",
+        "Taurus": "You might find unexpected financial gainns today.",
         "Gemini": "Take Care of your health.",
         "Cancer": "You might find someone special in your life.",
         "Leo": "Today you might get an increment in your job Salary.",
