4e6c0946d9bcc6a42df27a5f74dcc0a797fac62a
Kushagra Kumar <167355875+KushagraKumar04@users.noreply.github.com>
2024-08-21 18:08:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def get_daily_horoscope(sign, day):
         "Taurus": "You might find unexpected financial gainns today.",
         "Gemini": "Take Care of your health and be physically fit.",
         "Cancer": "You might find someone special in your life.",
-        "Leo": "Today you might get an increment in your job Salary.",
+        "Leo": "Today you might get an increment in your job .",
         # Add more signs and messages as needed.
     }
     
