af94babfd83e08c48e021088817d51a0ae8587f2
Kushagra Kumar <167355875+KushagraKumar04@users.noreply.github.com>
2024-09-04 17:48:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ def send_welcome(message):
 def sign_handler(message):
     text = ("What's your zodiac sign?\n"
             "Choose one: *Aries*, *Taurus*, *Gemini*, *Cancer*, *Leoo*, *Virgo*, "
-            "*Libra*, *Scorpio*, *Sagittarious*, *Capricorn*, *Aquarius*, and *Pisces*.")
+            "*Libra*, *Scorpio*, *Sagitarious*, *Capricorn*, *Aquarius*, and *Pisces*.")
     sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
     bot.register_next_step_handler(sent_msg, day_handler)
 
